0df3c99a141c8233448457f6dde47e83cf12286f
punisher21-bit <111782231+punisher21-bit@users.noreply.github.com>
2025-03-29 18:09:43 +0000

    Update random_words.cpp

--- random_words.cpp
+++ random_words.cpp
@@ -6,51 +6,52 @@
 using namespace std ;
 
 vector<string> split(string index){
-	fstream mine(index,ios::in | ios::out);
-	if( ! mine.is_open() ) system("PAUSE");
-	vector<string> split_index ;
-	string word ; 
-	while(mine >> word){
-		split_index.push_back(word);
-	}
-	mine.close();
-	return split_index ;
+        fstream mine(index,ios::in | ios::out);
+        if( ! mine.is_open() ) system("PAUSE");
+        vector<string> split_index ;
+        string word ;
+        while(mine >> word){
+                split_index.push_back(word);
+        }
+        mine.close();
+        return split_index ;
 }
 
 map<string,int> frequences(vector<string> split_index){
-	map<string,int> split_frequences ;
-	for(auto& k : now){
-		split_frequences[k]++;
-	}
-	return split_frequences;
+        map<string,int> split_frequences ;
+        for(auto& k : split_index){
+                split_frequences[k]++;
+        }
+        return split_frequences;
 }
 
 string random_word(vector<string> split_index, map<string,int> split_frequences){
-	int counter = 5 ; // only 5 words !
-	string rd_word = "";
-	srand(time(NULL));
-	while(counter--){
-		int rd_number = rand()%9 ;
-		if(split_frequences[split_index[rd_number]] > 0){
-			rd_word += now[rd_number] + " ";
-		}
-		else{
-			counter++;
-			continue ;
-		}
-			
-	}
-	return rd_word ;
+        int counter = 5 ; // only 5 words !
+        string rd_word = "";
+        srand(time(NULL));
+        while(counter--){
+                int rd_number = rand()%9 ;
+                if(split_frequences[split_index[rd_number]] > 0){
+                        rd_word += split_index[rd_number] + " ";
+                        split_frequences[split_index[rd_number]]-- ;
+                }
+                else{
+                        counter++;
+                        continue ;
+                }
+
+        }
+        return rd_word ;
 }
 
 int main(){
-	string index = "document.txt";
-	vector <string> split_index = split(index);
-	map<string,int> result = frequences(split_index);
-	for(auto& k : result){
-		cout << k.first << " : " << k.second << '\n';
-	}
-	string rd_word = random_word(now,result);
-	cout << rd_word << '\n';
-	return 0 ;
+        string index = "document.txt";
+        vector <string> split_index = split(index);
+        map<string,int> result = frequences(split_index);
+        for(auto& k : result){
+                cout << k.first << " : " << k.second << '\n';
+        }
+        string rd_word = random_word(split_index,result);
+        cout << rd_word << '\n';
+        return 0 ;
 }
